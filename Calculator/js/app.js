90e711c63a727880d107a0fb4b73049154411cf6
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-03-08 12:44:26 -0600

    Update app.js

--- Calculator/js/app.js
+++ Calculator/js/app.js
@@ -11,6 +11,49 @@ const nine = 9;
 let begValue = 0;
 let zeroCount = 0;
 let digCount = 0;
+let flMash = 0;
+
+document.getElementById(`zero`).addEventListener(`click`, function(){
+    if (digCount === 0){
+        document.getElementById(`firstLine--one`).innerHTML = begValue + zero;
+        digCount++;
+    }
+    else if (digCount === 1){
+        document.getElementById(`firstLine--two`).innerHTML = begValue + zero;
+        digCount++;
+    }
+    else if (digCount === 2){
+        document.getElementById(`firstLine--three`).innerHTML = begValue + zero;
+        digCount++;
+    }
+    else if (digCount === 3){
+        document.getElementById(`firstLine--four`).innerHTML = begValue + zero;
+        digCount++;
+    }
+    else if (digCount === 4){
+        document.getElementById(`firstLine--five`).innerHTML = begValue + zero;
+        digCount++;
+    }
+    else if (digCount === 5){
+        document.getElementById(`firstLine--six`).innerHTML = begValue + zero;
+        digCount++;
+    }
+    else if (digCount === 6){
+        document.getElementById(`firstLine--seven`).innerHTML = begValue + zero;
+        digCount++;
+    }
+    else if (digCount === 7){
+        document.getElementById(`firstLine--eight`).innerHTML = begValue + zero;
+        digCount++;
+    }
+    else if (digCount === 8){
+        document.getElementById(`firstLine--nine`).innerHTML = begValue + zero;
+        digCount++;
+    }
+    else if (digCount === 9){
+        digCount = 9;
+    }
+});
 
 document.getElementById(`one`).addEventListener(`click`, function(){
     if (digCount === 0){
@@ -94,4 +137,350 @@ document.getElementById(`two`).addEventListener(`click`, function(){
     else if (digCount === 9){
         digCount = 9;
     }
+});
+
+document.getElementById(`three`).addEventListener(`click`, function(){
+    if (digCount === 0){
+        document.getElementById(`firstLine--one`).innerHTML = begValue + three;
+        digCount++;
+    }
+    else if (digCount === 1){
+        document.getElementById(`firstLine--two`).innerHTML = begValue + three;
+        digCount++;
+    }
+    else if (digCount === 2){
+        document.getElementById(`firstLine--three`).innerHTML = begValue + three;
+        digCount++;
+    }
+    else if (digCount === 3){
+        document.getElementById(`firstLine--four`).innerHTML = begValue + three;
+        digCount++;
+    }
+    else if (digCount === 4){
+        document.getElementById(`firstLine--five`).innerHTML = begValue + three;
+        digCount++;
+    }
+    else if (digCount === 5){
+        document.getElementById(`firstLine--six`).innerHTML = begValue + three;
+        digCount++;
+    }
+    else if (digCount === 6){
+        document.getElementById(`firstLine--seven`).innerHTML = begValue + three;
+        digCount++;
+    }
+    else if (digCount === 7){
+        document.getElementById(`firstLine--eight`).innerHTML = begValue + three;
+        digCount++;
+    }
+    else if (digCount === 8){
+        document.getElementById(`firstLine--nine`).innerHTML = begValue + three;
+        digCount++;
+    }
+    else if (digCount === 9){
+        digCount = 9;
+    }
+});
+
+document.getElementById(`four`).addEventListener(`click`, function(){
+    if (digCount === 0){
+        document.getElementById(`firstLine--one`).innerHTML = begValue + four;
+        digCount++;
+    }
+    else if (digCount === 1){
+        document.getElementById(`firstLine--two`).innerHTML = begValue + four;
+        digCount++;
+    }
+    else if (digCount === 2){
+        document.getElementById(`firstLine--three`).innerHTML = begValue + four;
+        digCount++;
+    }
+    else if (digCount === 3){
+        document.getElementById(`firstLine--four`).innerHTML = begValue + four;
+        digCount++;
+    }
+    else if (digCount === 4){
+        document.getElementById(`firstLine--five`).innerHTML = begValue + four;
+        digCount++;
+    }
+    else if (digCount === 5){
+        document.getElementById(`firstLine--six`).innerHTML = begValue + four;
+        digCount++;
+    }
+    else if (digCount === 6){
+        document.getElementById(`firstLine--seven`).innerHTML = begValue + four;
+        digCount++;
+    }
+    else if (digCount === 7){
+        document.getElementById(`firstLine--eight`).innerHTML = begValue + four;
+        digCount++;
+    }
+    else if (digCount === 8){
+        document.getElementById(`firstLine--nine`).innerHTML = begValue + four;
+        digCount++;
+    }
+    else if (digCount === 9){
+        digCount = 9;
+    }
+});
+
+document.getElementById(`five`).addEventListener(`click`, function(){
+    if (digCount === 0){
+        document.getElementById(`firstLine--one`).innerHTML = begValue + five;
+        digCount++;
+    }
+    else if (digCount === 1){
+        document.getElementById(`firstLine--two`).innerHTML = begValue + five;
+        digCount++;
+    }
+    else if (digCount === 2){
+        document.getElementById(`firstLine--three`).innerHTML = begValue + five;
+        digCount++;
+    }
+    else if (digCount === 3){
+        document.getElementById(`firstLine--four`).innerHTML = begValue + five;
+        digCount++;
+    }
+    else if (digCount === 4){
+        document.getElementById(`firstLine--five`).innerHTML = begValue + five;
+        digCount++;
+    }
+    else if (digCount === 5){
+        document.getElementById(`firstLine--six`).innerHTML = begValue + five;
+        digCount++;
+    }
+    else if (digCount === 6){
+        document.getElementById(`firstLine--seven`).innerHTML = begValue + five;
+        digCount++;
+    }
+    else if (digCount === 7){
+        document.getElementById(`firstLine--eight`).innerHTML = begValue + five;
+        digCount++;
+    }
+    else if (digCount === 8){
+        document.getElementById(`firstLine--nine`).innerHTML = begValue + five;
+        digCount++;
+    }
+    else if (digCount === 9){
+        digCount = 9;
+    }
+});
+
+document.getElementById(`six`).addEventListener(`click`, function(){
+    if (digCount === 0){
+        document.getElementById(`firstLine--one`).innerHTML = begValue + six;
+        digCount++;
+    }
+    else if (digCount === 1){
+        document.getElementById(`firstLine--two`).innerHTML = begValue + six;
+        digCount++;
+    }
+    else if (digCount === 2){
+        document.getElementById(`firstLine--three`).innerHTML = begValue + six;
+        digCount++;
+    }
+    else if (digCount === 3){
+        document.getElementById(`firstLine--four`).innerHTML = begValue + six;
+        digCount++;
+    }
+    else if (digCount === 4){
+        document.getElementById(`firstLine--five`).innerHTML = begValue + six;
+        digCount++;
+    }
+    else if (digCount === 5){
+        document.getElementById(`firstLine--six`).innerHTML = begValue + six;
+        digCount++;
+    }
+    else if (digCount === 6){
+        document.getElementById(`firstLine--seven`).innerHTML = begValue + six;
+        digCount++;
+    }
+    else if (digCount === 7){
+        document.getElementById(`firstLine--eight`).innerHTML = begValue + six;
+        digCount++;
+    }
+    else if (digCount === 8){
+        document.getElementById(`firstLine--nine`).innerHTML = begValue + six;
+        digCount++;
+    }
+    else if (digCount === 9){
+        digCount = 9;
+    }
+});
+
+document.getElementById(`seven`).addEventListener(`click`, function(){
+    if (digCount === 0){
+        document.getElementById(`firstLine--one`).innerHTML = begValue + seven;
+        digCount++;
+    }
+    else if (digCount === 1){
+        document.getElementById(`firstLine--two`).innerHTML = begValue + seven;
+        digCount++;
+    }
+    else if (digCount === 2){
+        document.getElementById(`firstLine--three`).innerHTML = begValue + seven;
+        digCount++;
+    }
+    else if (digCount === 3){
+        document.getElementById(`firstLine--four`).innerHTML = begValue + seven;
+        digCount++;
+    }
+    else if (digCount === 4){
+        document.getElementById(`firstLine--five`).innerHTML = begValue + seven;
+        digCount++;
+    }
+    else if (digCount === 5){
+        document.getElementById(`firstLine--six`).innerHTML = begValue + seven;
+        digCount++;
+    }
+    else if (digCount === 6){
+        document.getElementById(`firstLine--seven`).innerHTML = begValue + seven;
+        digCount++;
+    }
+    else if (digCount === 7){
+        document.getElementById(`firstLine--eight`).innerHTML = begValue + seven;
+        digCount++;
+    }
+    else if (digCount === 8){
+        document.getElementById(`firstLine--nine`).innerHTML = begValue + seven;
+        digCount++;
+    }
+    else if (digCount === 9){
+        digCount = 9;
+    }
+});
+
+document.getElementById(`eight`).addEventListener(`click`, function(){
+    if (digCount === 0){
+        document.getElementById(`firstLine--one`).innerHTML = begValue + eight;
+        digCount++;
+    }
+    else if (digCount === 1){
+        document.getElementById(`firstLine--two`).innerHTML = begValue + eight;
+        digCount++;
+    }
+    else if (digCount === 2){
+        document.getElementById(`firstLine--three`).innerHTML = begValue + eight;
+        digCount++;
+    }
+    else if (digCount === 3){
+        document.getElementById(`firstLine--four`).innerHTML = begValue + eight;
+        digCount++;
+    }
+    else if (digCount === 4){
+        document.getElementById(`firstLine--five`).innerHTML = begValue + eight;
+        digCount++;
+    }
+    else if (digCount === 5){
+        document.getElementById(`firstLine--six`).innerHTML = begValue + eight;
+        digCount++;
+    }
+    else if (digCount === 6){
+        document.getElementById(`firstLine--seven`).innerHTML = begValue + eight;
+        digCount++;
+    }
+    else if (digCount === 7){
+        document.getElementById(`firstLine--eight`).innerHTML = begValue + eight;
+        digCount++;
+    }
+    else if (digCount === 8){
+        document.getElementById(`firstLine--nine`).innerHTML = begValue + eight;
+        digCount++;
+    }
+    else if (digCount === 9){
+        digCount = 9;
+    }
+});
+
+document.getElementById(`nine`).addEventListener(`click`, function(){
+    if (digCount === 0){
+        document.getElementById(`firstLine--one`).innerHTML = begValue + nine;
+        digCount++;
+    }
+    else if (digCount === 1){
+        document.getElementById(`firstLine--two`).innerHTML = begValue + nine;
+        digCount++;
+    }
+    else if (digCount === 2){
+        document.getElementById(`firstLine--three`).innerHTML = begValue + nine;
+        digCount++;
+    }
+    else if (digCount === 3){
+        document.getElementById(`firstLine--four`).innerHTML = begValue + nine;
+        digCount++;
+    }
+    else if (digCount === 4){
+        document.getElementById(`firstLine--five`).innerHTML = begValue + nine;
+        digCount++;
+    }
+    else if (digCount === 5){
+        document.getElementById(`firstLine--six`).innerHTML = begValue + nine;
+        digCount++;
+    }
+    else if (digCount === 6){
+        document.getElementById(`firstLine--seven`).innerHTML = begValue + nine;
+        digCount++;
+    }
+    else if (digCount === 7){
+        document.getElementById(`firstLine--eight`).innerHTML = begValue + nine;
+        digCount++;
+    }
+    else if (digCount === 8){
+        document.getElementById(`firstLine--nine`).innerHTML = begValue + nine;
+        digCount++;
+    }
+    else if (digCount === 9){
+        digCount = 9;
+    }
+});
+
+document.getElementById(`add`).addEventListener(`click`, function(){
+
+    if (isNaN(parseFloat(document.getElementById(`firstLine--one`).innerHTML))){
+        flMash = 0;
+        console.log(`NaNTrue`);
+    }
+    if (isNaN(parseFloat(document.getElementById(`firstLine--two`).innerHTML))){
+        flMash = parseFloat(document.getElementById(`firstLine--one`).innerHTML);
+        console.log(flMash);
+        console.log(`NaNTrue`);
+    }
+    if (isNaN(parseFloat(document.getElementById(`firstLine--three`).innerHTML))){
+        flMash = `${parseFloat(document.getElementById(`firstLine--one`).innerHTML)}${parseFloat(document.getElementById(`firstLine--two`).innerHTML)}`
+        console.log(flMash);
+        console.log(`NaNTrue`);
+    }
+    if (isNaN(parseFloat(document.getElementById(`firstLine--four`).innerHTML))){
+        flMash = `${parseFloat(document.getElementById(`firstLine--one`).innerHTML)}${parseFloat(document.getElementById(`firstLine--two`).innerHTML)}${parseFloat(document.getElementById(`firstLine--three`).innerHTML)}`
+        console.log(flMash);
+        console.log(`NaNTrue`);
+    }
+    if (isNaN(parseFloat(document.getElementById(`firstLine--five`).innerHTML))){
+        console.log(flMash);
+        console.log(`NaNTrue`);
+    }
+    if (isNaN(parseFloat(document.getElementById(`firstLine--six`).innerHTML))){
+        console.log(flMash);
+        console.log(`NaNTrue`);
+    }
+    if (isNaN(parseFloat(document.getElementById(`firstLine--seven`).innerHTML))){
+        console.log(flMash);
+        console.log(`NaNTrue`);
+    }
+    if (isNaN(parseFloat(document.getElementById(`firstLine--eight`).innerHTML))){
+        console.log(flMash);
+        console.log(`NaNTrue`);
+    }
+    if (isNaN(parseFloat(document.getElementById(`firstLine--nine`).innerHTML))){
+        console.log(flMash);
+        console.log(`NaNTrue`);
+    }
+    
+    /*if (parseFloat(document.getElementById(`firstLine--two`).innerHTML) !== ){
+        return;
+    } 
+    else if (parseFloat(document.getElementById(`firstLine--two`).innerHTML) !== NaN){
+        flMash = `${parseFloat(document.getElementById(`firstLine--one`).innerHTML)}${parseFloat(document.getElementById(`firstLine--two`).innerHTML)}`
+        console.log(flMash);
+    }
+    */
 });
